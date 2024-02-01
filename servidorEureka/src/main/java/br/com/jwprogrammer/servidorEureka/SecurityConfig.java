dbf5c178af5aeef4f06176d621cf872180b7645f
Josias Wando Nobre de Queiroz <josiasn.queiroz@gmail.com>
2024-01-31 21:52:14 -0300

    Segurança do eureka server.

---
+++ servidorEureka/src/main/java/br/com/jwprogrammer/servidorEureka/SecurityConfig.java
@@ -0,0 +1,22 @@
+package br.com.jwprogrammer.servidorEureka;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests( auth ->
+                        auth.anyRequest().authenticated()
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+}
